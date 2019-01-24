917bb34bd37477f65c18cb131c5228dbd01b946e
David Buhauer <db@idealdev.dk>
2019-01-24 21:32:11 +0100

    first commit

---
+++ Sources/App/Configuration/configure.swift
@@ -0,0 +1,26 @@
+import FluentPostgreSQL
+import Vapor
+
+public func configure(
+    _ config: inout Config,
+    _ env: inout Environment,
+    _ services: inout Services
+    ) throws {
+    try services.register(FluentPostgreSQLProvider())
+    
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+    
+    let middlewares = MiddlewareConfig.default()
+    services.register(middlewares)
+    
+    var databases = DatabasesConfig()
+    let config = PostgreSQLDatabaseConfig(hostname: "localhost", username: "davidbuhauer", database: "chatter")
+    databases.add(database: PostgreSQLDatabase(config: config), as: .psql)
+    services.register(databases)
+    
+    var migrations = MigrationConfig()
+    migrations.add(model: User.self, database: .psql)
+    services.register(migrations)
+}
---
+++ Sources/App/Configuration/routes.swift
@@ -0,0 +1,16 @@
+import Routing
+import Vapor
+
+public func routes(_ router: Router) throws {
+    router.get { (request) in
+        return "Hello Vapor!"
+    }
+    
+    router.post(User.self, at: "users") { (request, user) in
+        return user.save(on: request)
+    }
+    
+    router.get("users") { req in
+        return User.query(on: req).all()
+    }
+}
