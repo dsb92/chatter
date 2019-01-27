c911a300a1f245576c650cf0b539e949fda4869b
David Buhauer <db@idealdev.dk>
2019-01-27 21:24:39 +0100

    Running Vapor live!

--- Sources/App/Configuration/routes.swift
+++ Sources/App/Configuration/routes.swift
@@ -3,4 +3,7 @@ import Vapor
 
 public func routes(_ router: Router) throws {
     try router.register(collection: UserController())
+    router.get { (request) in
+        return "Running Vapor!"
+    }
 }
